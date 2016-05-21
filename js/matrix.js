cdd2d981c3e4796b3100d89e7e2187605c3abe25
Shipeng Xu <billhsu.x@gmail.com>
2016-05-21 12:45:21 +0800

    refactor renderer.js

--- js/matrix.js
+++ js/matrix.js
@@ -15,28 +15,13 @@ var hasFloat32Array = (typeof Float32Array != 'undefined');
 function Matrix() {
     var m = Array.prototype.concat.apply([], arguments);
     if (!m.length) {
-        m = [
-            1,
-            0,
-            0,
-            0,
-            0,
-            1,
-            0,
-            0,
-            0,
-            0,
-            1,
-            0,
-            0,
-            0,
-            0,
-            1
-        ];
+        m = [1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1];
+
     }
-    this.m = hasFloat32Array
-        ? new Float32Array(m)
-        : m;
+    this.m = hasFloat32Array ? new Float32Array(m) : m;
 }
 
 Matrix.prototype = {
--- js/renderer.js
+++ js/renderer.js
@@ -13,33 +13,8 @@ function Renderer(width, height) {
     this.clearBuffer(this.zbuffer);
 }
 Renderer.prototype = {
-    barycentric: function(a, b, c, p) {
-        var s0 = new Vector(c.x - a.x, b.x - a.x, a.x - p.x);
-        var s1 = new Vector(c.y - a.y, b.y - a.y, a.y - p.y);
-        var u = s0.cross(s1);
-        if (Math.abs(u.z) > 1e-2) {
-            return new Vector(1 - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
-        }
-        return new Vector(-1, 1, 1);
-    },
     drawTrangle: function(a, b, c, color) {
-        var ta = this.viewportMatrix.transformPoint(a);
-        ta = ta.divide(ta.w);
-        var tb = this.viewportMatrix.transformPoint(b);
-        tb = tb.divide(tb.w);
-        var tc = this.viewportMatrix.transformPoint(c);
-        tc = tc.divide(tc.w);
-        var canvas = document.getElementById('canvas');
-        if (canvas.getContext) {
-            var ctx = canvas.getContext('2d');
-            ctx.beginPath();
-            ctx.moveTo(ta.x, ta.y);
-            ctx.lineTo(tb.x, tb.y);
-            ctx.lineTo(tc.x, tc.y);
-            ctx.closePath();
-            ctx.fillStyle = color;
-            ctx.fill();
-        }
+        Renderer.drawTrangle(this.viewportMatrix, a, b, c, color);
     },
     clearBuffer: function(buffer) {
         for (var i = 0; i < this.width * this.height; ++i) {
@@ -47,54 +22,13 @@ Renderer.prototype = {
         }
     },
     viewport: function(x, y) {
-        var matrix = new Matrix();
-        matrix.m[0] = this.width / 2;
-        matrix.m[1] = 0;
-        matrix.m[2] = 0;
-        matrix.m[3] = x + this.width / 2;
-        matrix.m[4] = 0;
-        matrix.m[5] = -this.height / 2;
-        matrix.m[6] = 0;
-        matrix.m[7] = y + this.height / 2;
-        matrix.m[8] = 0;
-        matrix.m[9] = 0;
-        matrix.m[10] = 0;
-        matrix.m[11] = 1;
-        matrix.m[12] = 0;
-        matrix.m[13] = 0;
-        matrix.m[14] = 0;
-        matrix.m[15] = 1;
-        return matrix;
+        return Renderer.viewport(x, y, this.width, this.height);
     },
     lookat: function(eye, center, up) {
-        up = up.unit();
-        var f = center.subtract(eye).unit();
-        var s = f.cross(up);
-        var u = s.unit().cross(f);
-        var matrix = new Matrix();
-        matrix.m[0] = s.x;
-        matrix.m[1] = s.y;
-        matrix.m[2] = s.z;
-
-        matrix.m[4] = u.x;
-        matrix.m[5] = u.y;
-        matrix.m[6] = u.z;
-
-        matrix.m[8] = -f.x;
-        matrix.m[9] = -f.y;
-        matrix.m[10] = -f.z;
-        return matrix.translate(eye.negative());
+        return Renderer.lookat(eye, center, up);
     },
     project: function(fovy, zNear, zFar) {
-        var f = 1 / Math.tan(fovy * Math.PI / 180 / 2);
-        var aspect = this.width / this.height;
-        var matrix = new Matrix();
-        matrix.m[0] = f / aspect;
-        matrix.m[5] = f;
-        matrix.m[10] = (zFar + zNear) / (zNear - zFar);
-        matrix.m[11] = 2 * zFar * zNear / (zNear - zFar);
-        matrix.m[14] = -1;
-        return matrix;
+        return Renderer.project(fovy, zNear, zFar, this.width, this.height);
     },
     resize: function(width, height) {
         this.width = width;
@@ -102,4 +36,86 @@ Renderer.prototype = {
         this.clearBuffer(this.zbuffer);
         this.viewportMatrix = this.viewport(0, 0);
     }
+}
+Renderer.drawTrangle = function(viewportMatrix, a, b, c, color) {
+    var ta = viewportMatrix.transformPoint(a);
+    ta = ta.divide(ta.w);
+    var tb = viewportMatrix.transformPoint(b);
+    tb = tb.divide(tb.w);
+    var tc = viewportMatrix.transformPoint(c);
+    tc = tc.divide(tc.w);
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.moveTo(ta.x, ta.y);
+        ctx.lineTo(tb.x, tb.y);
+        ctx.lineTo(tc.x, tc.y);
+        ctx.closePath();
+        ctx.fillStyle = color;
+        ctx.fill();
+    }
+}
+
+Renderer.barycentric = function(a, b, c, p) {
+    var s0 = new Vector(c.x - a.x, b.x - a.x, a.x - p.x);
+    var s1 = new Vector(c.y - a.y, b.y - a.y, a.y - p.y);
+    var u = s0.cross(s1);
+    if (Math.abs(u.z) > 1e-2) {
+        return new Vector(1 - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+    }
+    return new Vector(-1, 1, 1);
+}
+
+Renderer.viewport = function(x, y, width, height) {
+    var matrix = new Matrix();
+    matrix.m[0] = width / 2;
+    matrix.m[1] = 0;
+    matrix.m[2] = 0;
+    matrix.m[3] = x + width / 2;
+    matrix.m[4] = 0;
+    matrix.m[5] = -height / 2;
+    matrix.m[6] = 0;
+    matrix.m[7] = y + height / 2;
+    matrix.m[8] = 0;
+    matrix.m[9] = 0;
+    matrix.m[10] = 0;
+    matrix.m[11] = 1;
+    matrix.m[12] = 0;
+    matrix.m[13] = 0;
+    matrix.m[14] = 0;
+    matrix.m[15] = 1;
+    return matrix;
+}
+
+Renderer.lookat = function(eye, center, up) {
+    up = up.unit();
+    var f = center.subtract(eye).unit();
+    var s = f.cross(up);
+    var u = s.unit().cross(f);
+    var matrix = new Matrix();
+    matrix.m[0] = s.x;
+    matrix.m[1] = s.y;
+    matrix.m[2] = s.z;
+
+    matrix.m[4] = u.x;
+    matrix.m[5] = u.y;
+    matrix.m[6] = u.z;
+
+    matrix.m[8] = -f.x;
+    matrix.m[9] = -f.y;
+    matrix.m[10] = -f.z;
+    return matrix.translate(eye.negative());
+}
+
+Renderer.project = function(fovy, zNear, zFar, width, height) {
+    var f = 1 / Math.tan(fovy * Math.PI / 180 / 2);
+    var aspect = width / height;
+    var matrix = new Matrix();
+    matrix.m[0] = f / aspect;
+    matrix.m[5] = f;
+    matrix.m[10] = (zFar + zNear) / (zNear - zFar);
+    matrix.m[11] = 2 * zFar * zNear / (zNear - zFar);
+    matrix.m[14] = -1;
+    return matrix;
 }
